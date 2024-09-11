44e8745f4bc1a374a8e027fe60f426071cd7d77b
Matheus Sleutjes <matheus.sleutjes@loglabdigital.com.br>
2024-09-10 20:48:08 -0400

    ajuste implementação

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,38 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		//filterString
+		List<String> strings = List.of("apple", "banana", "cherry", "date");
+        
+        Predicate<String> startsWithA = str -> str.startsWith("a");
+        
+        List<String> filteredList = filterStrings(strings, startsWithA);
+
+		//removeNegativeNumbers
+
+		List<Integer> numbers = List.of(-5, 3, -1, 7, 0, -12, 9);
+
+		removeNegativeNumbers(numbers );
+
+		//findFirstMatch
+		List<Integer> numbers2 = List.of(-5, 3, -1, 7, 0, -12, 9);
+
+        Predicate<Integer> isPositive = number -> number > 0;
+
+        Integer result = findFirstMatch(numbers2, isPositive);
 	}
 
-	private List<String> filterStrings(List<String> lista, Predicate<String> predicate){
+	private static List<String> filterStrings(List<String> lista, Predicate<String> predicate){
 		return lista.stream()
                     .filter(predicate)
                     .collect(Collectors.toList());
 	}
 
-	private List<Integer> removeNegativeNumbers(List<Integer> numbers, Predicate<Integer> predicate) {
-        numbers.removeIf(predicate);
-		return numbers;
+	private static void removeNegativeNumbers(List<Integer> numbers) {
+        numbers.removeIf(number -> number < 0);
     }
 
-	private Integer findFirstMatch(List<Integer> numbers, Predicate<Integer> predicate) {
+	private static Integer findFirstMatch(List<Integer> numbers, Predicate<Integer> predicate) {
         for (Integer number : numbers) {
             if (predicate.test(number)) {
                 return number;
